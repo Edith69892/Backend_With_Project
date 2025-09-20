2635af5b1219a91af9725c5fd8032b2a2498c9f1
Roshan Patil <patilroshan9687@gmail.com>
2025-09-20 10:43:14 +0530

    some comment add

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -8,3 +8,5 @@ class ApiResponse{
 }
 
 export {ApiResponse}
+
+// ApiResponse.js is used to send the response to the client. It is a class that takes statusCode, message and data as arguments. It sets the success property to true if the statusCode is less than 400, otherwise it sets it to false. It also sets the message and data properties. This way we can have a consistent response format for all the APIs.
