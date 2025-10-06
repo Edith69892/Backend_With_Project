d1512ab74ad5c062bb3253a9505e4fc2e9bbcd56
Roshan Patil <patilroshan9687@gmail.com>
2025-10-06 23:26:12 +0530

    fix the heallthCheck.controller.js name

--- src/controllers/healthCheck.controler.js
+++ src/controllers/healthCheck.controler.js
@@ -1,7 +1,7 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
-import { ApiError } from "../utils/ApiError";
-import { ApiResponse } from "../utils/ApiResponse";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 const healthCheck = asyncHandler(async (req, res) => {
--- src/routes/healthCheck.routes.js
+++ src/routes/healthCheck.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { healthCheck } from "../controllers/heakcheck.controler";
+import { healthCheck } from "../controllers/healthCheck.controler";
 
 const router = Router()
 
