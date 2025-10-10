f8de89c082c4e3360ff60479322200e7b8b08ac4
Roshan Patil <patilroshan9687@gmail.com>
2025-10-10 17:44:52 +0530

    write toggleVideoLike and toggleCommentLike

--- src/controllers/likes.controller.js
+++ src/controllers/likes.controller.js
@@ -1,5 +1,6 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { Like } from "../models/like.model.js";
+import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -19,57 +20,121 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
     throw new ApiError(400, "video not found");
   }
 
-//   const isLiked = await Like.findOne({ video: videoId, likedBy: req.user._id });
+  //   const isLiked = await Like.findOne({ video: videoId, likedBy: req.user._id });
 
-//   if (isLiked) {
-//         await Like.findByIdAndDelete(isLiked?._id);
-//         await Video.findByIdAndUpdate(videoId, {
-//         $inc: { likesCount: -1 },
-//         });
+  //   if (isLiked) {
+  //         await Like.findByIdAndDelete(isLiked?._id);
+  //         await Video.findByIdAndUpdate(videoId, {
+  //         $inc: { likesCount: -1 },
+  //         });
 
-//         message = "Unlike successfull";
+  //         message = "Unlike successfull";
 
-//   }
-//    else {
-//         await Like.create({ video: videoId, likedBy: req.user._id });
-//         await Video.findByIdAndUpdate(video._id, {
-//         $inc: { likesCount: 1 },
-//         });
-//         message = "like successfull";
-//   }
+  //   }
+  //    else {
+  //         await Like.create({ video: videoId, likedBy: req.user._id });
+  //         await Video.findByIdAndUpdate(video._id, {
+  //         $inc: { likesCount: 1 },
+  //         });
+  //         message = "like successfull";
+  //   }
 
-//   return res
-//     .status(200)
-//     .json(new ApiResponse(200, { likesCount: video.likesCount }, message));
+  //   return res
+  //     .status(200)
+  //     .json(new ApiResponse(200, { likesCount: video.likesCount }, message));
 
-    const likedCheck = await Like.findOne({ video: videoId, likedBy: req.user._id });
+  const likedCheck = await Like.findOne({
+    video: videoId,
+    likedBy: req.user._id,
+  });
 
-    if(likedCheck){
-        await Like.findByIdAndDelete(isLiked?._id);
-        await Video.findByIdAndUpdate(videoId, {
-        $inc: { likesCount: -1 },
-        });
+  if (likedCheck) {
+    await Like.findByIdAndDelete(isLiked?._id);
+    const updatedVideo = await Video.findByIdAndUpdate(
+      videoId,
+      { $inc: { likesCount: -1 } },
+      { new: true }
+    );
 
-        return res
-        .status(200)
-        .json( new ApiResponse(200, "Unliked Successfull", { isLiked : false, likesCount : video.likesCount}))
+    return res.status(200).json(
+      new ApiResponse(200, "Unliked Successfull", {
+        isLiked: false,
+        likesCount: updatedVideo.likesCount,
+      })
+    );
+  }
 
-    }
+  await Like.create({ video: videoId, likedBy: req.user._id });
+  const updatedVideo = await Video.findByIdAndUpdate(
+    videoId,
+    { $inc: { likesCount: 1 } },
+    { new: true }
+  );
 
-        await Like.create({ video: videoId, likedBy: req.user._id });
-        await Video.findByIdAndUpdate(video._id, {
-        $inc: { likesCount: 1 },
-        });
-
-        return res
-        .status(200)
-        .json( new ApiResponse(200, "liked Successfull", { isLiked : true, likesCount : video.likesCount}))
-    
+  return res.status(200).json(
+    new ApiResponse(200, "liked Successfull", {
+      isLiked: true,
+      likesCount: updatedVideo.likesCount,
+    })
+  );
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
   //TODO: toggle like on comment
+
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Comment Id is Invalid");
+  }
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new ApiError(400, "video not found");
+  }
+
+  const likedCheck = await Like.findOne({
+    comment: commentId,
+    likedBy: req.user._id,
+  });
+
+  if (likedCheck) {
+    await Like.findByIdAndDelete(likedCheck?._id);
+    const updatedComment = await Comment.findByIdAndUpdate(
+      commentId,
+      {
+        $inc: { likesCount: -1 },
+      },
+      {
+        new: true,
+      }
+    );
+
+    return res.status(200).json(
+      new ApiResponse(200, "Unliked Successfull", {
+        isLiked: false,
+        likesCount: updatedComment.likesCount,
+      })
+    );
+  }
+
+  await Like.create({ comment: commentId, likedBy: req.user._id });
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $inc: { likesCount: 1 },
+    },
+    {
+      new: true,
+    }
+  );
+
+  return res.status(200).json(
+    new ApiResponse(200, "liked Successfull", {
+      isLiked: true,
+      likesCount: updatedComment.likesCount,
+    })
+  );
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
