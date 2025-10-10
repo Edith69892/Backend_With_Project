66508ff3d7a87345b4e9e22b5e671ba59affbc8b
Roshan Patil <patilroshan9687@gmail.com>
2025-10-10 17:43:42 +0530

    add likesCount

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,26 +1,30 @@
-import mongoose, {Schema} from "mongoose"; 
-import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2" 
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commentSchema = new Schema(
-    {
-        content : {
-            type : String,
-            require : true
-        },
-        video : {
-            type : Schema.Types.ObjectId,
-            ref : "Video"
-        },
-        owner : {
-            type : Schema.Types.ObjectId,
-            ref : "User"
-        }
+  {
+    content: {
+      type: String,
+      require: true,
     },
-    {
-        timestamps : true
-    }
-)
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    likesCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 commentSchema.plugin(mongooseAggregatePaginate);
 
-export const Comment = mongoose.model("Comment", commentSchema)
+export const Comment = mongoose.model("Comment", commentSchema);
