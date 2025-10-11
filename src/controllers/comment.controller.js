8440374ca3ec4e6c4535a6d90e61cdd692a2e5c8
Roshan Patil <patilroshan9687@gmail.com>
2025-10-11 18:39:30 +0530

    fetch video comments with owner info, likesCount, isLiked, sorted by newest

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,107 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Video Id is Invalid");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(400, "video not found");
+  }
+
+  const AggregateComments = Comment.aggregate([
+    {
+      $match: {
+        video: video._id,
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              userName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$owner",
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "comment",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likes.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $sort: { createdAt: -1 },
+    },
+    {
+      $project: {
+        content: 1,
+        owner: "$owner",
+        isLiked: 1,
+        likesCount: 1,
+        createdAt: 1,
+      },
+    },
+  ]);
+
+  const options = {
+    page: parseInt(page, 10),
+    limit: parseInt(limit, 10),
+  };
+  const comments = await Comment.aggregatePaginate(AggregateComments, options);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comments, "Comments Fetched Successfully."));
+});
+
+const addComment = asyncHandler(async (req, res) => {
+  // TODO: add a comment to a video
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  // TODO: update a comment
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  // TODO: delete a comment
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
