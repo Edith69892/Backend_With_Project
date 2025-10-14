d3dacb019c2239ac73cbad3bc58fe8dca7758b2a
Roshan Patil <patilroshan9687@gmail.com>
2025-10-15 00:44:11 +0530

    add dashboard routes for channel statistics and videos with user verification

---
+++ src/routes/dashboard.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { getChannelStats, getChannelVideos } from "../controllers/dashboard.controller.js"
+
+import { verifyUser } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.use(verifyUser);
+
+router.route("/stats").get(getChannelStats);
+router.route("/videos").get(getChannelVideos)
+
+export default router;
