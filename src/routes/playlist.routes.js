4a77a50cd0934802bc8b01963def0d63203fa0e7
Roshan Patil <patilroshan9687@gmail.com>
2025-10-15 17:43:33 +0530

    fix: replace verifyJWT middleware with verifyUser in playlist routes

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -9,11 +9,11 @@ import {
     removeVideoFromPlaylist,
     updatePlaylist,
 } from "../controllers/playlist.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+import {verifyUser} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+router.use(verifyUser); // Apply verifyUser middleware to all routes in this file
 
 router.route("/").post(createPlaylist)
 
