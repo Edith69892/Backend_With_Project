1d971341866e329bddda60771befa23d71450b24
Roshan Patil <patilroshan9687@gmail.com>
2025-10-15 15:50:39 +0530

    implement playlist management functionality with create and fetch operations

---
+++ src/controllers/playlist.controller.js
@@ -0,0 +1,92 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Playlist } from "../models/playlist.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+
+//TODO: create playlist
+const createPlaylist = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+
+  if (!name) {
+    throw new ApiError(400, "Playlist name is required.");
+  }
+
+  if (!isValidObjectId(req.user?._id)) {
+    throw new ApiError(400, "You must be logged in to create a playlist.");
+  }
+
+  const playlist = await Playlist.create({
+    name: name.trim(),
+    description: description?.trim() || "",
+    owner: req.user._id,
+    videos: [],
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, { playlist }, "Playlist created successfully."));
+});
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  //TODO: get user playlists
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid userId.");
+  }
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(404, "User not found.");
+  }
+
+  const playlists = await Playlist.find({ owner: userId });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { totalPlaylists: playlists.length, playlists },
+        "Playlist Fetched Successfully."
+      )
+    );
+});
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+  //TODO: get playlist by id
+});
+
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+  const { playlistId, videoId } = req.params;
+});
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
+  const { playlistId, videoId } = req.params;
+  // TODO: remove video from playlist
+});
+
+const deletePlaylist = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+  // TODO: delete playlist
+});
+
+const updatePlaylist = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+  const { name, description } = req.body;
+  //TODO: update playlist
+});
+
+export {
+  createPlaylist,
+  getUserPlaylists,
+  getPlaylistById,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  deletePlaylist,
+  updatePlaylist,
+};
